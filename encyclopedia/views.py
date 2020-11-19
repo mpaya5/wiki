6217fe08dd4e16cb506ca6d0cc0d1caaec61d3ad
Miguel Paya <miguelpaya@Miguels-MacBook-Air.local>
2020-11-19 11:38:29 +0100

    I've finished the first two tasks to do: the Entry Page and the Index Page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,31 @@
+import random
+
 from django.shortcuts import render
 
 from . import util
 
+from markdown2 import Markdown
+
+markdwn = Markdown()
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
+    return render(request, "encyclopedia/index.html",{
         "entries": util.list_entries()
     })
+        
+    
+    
 
+
+def entry(request, title):
+    entries = util.list_entries()
+    if title in entries:
+        page = util.get_entry(title)
+        page_converted = markdwn.convert(page) 
+        context = {
+            'page': page_converted,
+            'title': title,
+        }
+        return render(request, "encyclopedia/entry.html", context)
+    else:
+        return render(request, "encyclopedia/error.html", {"message": "The requested page was not found."})
